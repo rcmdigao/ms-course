2f477ac8615c9242c17cc524b7ddf741f4a2bca4
Rodrigo Cunha Machado <rodrigo.cunham@sonda.com>
2022-11-18 16:51:01 -0300

    Rest Tempalte

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,37 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workedId, int days){
-        return new Payment("Bob", 200.0, days);
+    // Pegando o valor de dentro do applicastion.properties
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(long workedId, int days) {
+
+        // Map > Dicionario de parametros
+        Map<String, String> uriVariables = new HashMap<>();
+
+        // Adicionando o parametro ID da url
+        uriVariables.put("id", "" + workedId + "");
+
+        // Comunicacao entre dois projetos
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables );
+
+        // Preenchimento dinamico
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
